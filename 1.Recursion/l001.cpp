5c4ade107eaab7ca482e45125d1ead13a8c28435
prateekjain07 <prateekjnwork@gmail.com>
2021-01-29 14:51:32 +0530

    First commit

---
+++ 1.Recursion/l001.cpp
@@ -0,0 +1,13 @@
+//g++ l001.cpp -o out && ./out > output.txt
+#include<iostream>
+using namespace std;
+
+void solve(){
+    cout<<"Ssup";
+}
+
+int main(){
+    solve();
+    return 0;
+
+}
---
+++ 1.Recursion/l001.java
@@ -0,0 +1,15 @@
+//javac l001.java && java l001 > output.txt
+public class l001{
+    public static void solve() {
+        System.out.println("Suup");
+    }
+    public static void main(String[] args) {
+        solve();
+    }
+    public static void sysoln(String a) {
+        System.out.println(a);
+    }
+    public static void syso(String a) {
+        System.out.print(a);
+    }
+}
