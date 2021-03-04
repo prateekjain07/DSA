7e2185560cc6557f30eefd24024b886e44d65ede
prateekjain07 <prateekjnwork@gmail.com>
2021-03-05 03:27:31 +0530

    Something

--- 1.Recursion/l003_PnC.java
+++ 1.Recursion/l003_PnC.java
@@ -1,6 +1,6 @@
 class l003_PnC{
 
-
+    static int calls= 0;
     public static void main(String[] args) {
         // CoinChangePnC();
         NQueenPnC();
@@ -27,20 +27,207 @@ class l003_PnC{
         //NQueenNoKill
         int m = boxes2D.length;
         int n = boxes2D[0].length;
+
+        calls = 0;
         System.out.println("CombSummationNQueenNoKill:  " + NQueenCombNoKill_Summation(que,boxes2D,0,0,""));
-        System.out.println();
+        System.out.println("Call: " + calls);
         // System.out.println("PermSummationNQueenNoKill:  " + NQueenPermNoKill_Summation(que,boxes2D,0,0,""));
         // System.out.println();
+        calls = 0;
+
         System.out.println("CombSummationNQueenNoKill_Optimised:  " + NQueenCombNoKill_Summation_02(que,boxes2D,0,0,""
         ,new boolean[m],new boolean[n],new boolean[m+n-1],new boolean[m+n-1]));
 
+        System.out.println("Call: " + calls);
+        
+        calls = 0;
+
+        System.out.println("CombSummationNQueenNoKill_OptimisedSubseq:  " + NQueenCombNoKill_Subseq_02(que,boxes2D,0,0,""
+        ,new boolean[m],new boolean[n],new boolean[m+n-1],new boolean[m+n-1]));
+        
+        System.out.println("Call: " + calls);
+        
+        calls = 0;
+
+        System.out.println("CombSummationNQueenNoKill_Optimised(PerRow)Summation:  " + NQueenCombNoKill_Summation_03(que,boxes2D,0,0,""
+        ,new boolean[m],new boolean[n],new boolean[m+n-1],new boolean[m+n-1]));
+
+        System.out.println("Call: " + calls);
+        
+        calls = 0;
+
+        System.out.println("CombSummationNQueenNoKill_Optimised(PerRow)SummationWithBits:  " + NQueenCombNoKill_Summation_04Bits(que,boxes2D,0,0,""
+        ,0,0,0,0));
+
+        System.out.println("Call: " + calls);
+        
+
+        //Adding a Static Variable Call to figure out how many calls are required in each function.
     }
 
 //qpsf - Queen placed so far
 //NoQ - Num of Queen
 
+public static int NQueenCombNoKill_Summation_04Bits(int NoQ, int[][] boxes, int idx, int qpsf, String ans,
+    int rows, int cols, int mainD, int otherD) {
+        calls++;
+        //Optimised Function wihtout needing to use isSafeToPlaceQueen function
+        //Don't even need the 2D Box now
+        
+        //To make this function for Permutation, 
+        //just replace int i = 0 instead of idx, basically idx becomes obsolete
+        
+        //Addition to previous codes, there's one queen per row.
+        //Boxes array is still useless here.
+        //Mapping: Q0 => R0
+        //Q1 => R1
+        //and so on 
+        //QPSF variable can be used to keep this track.
+
+        //Addition is that since now, we'll use Bits instead of boolean Arrays,
+        //all of the operations in finding shadow values will become O(1) and 
+        //also the space will become O(1) as only 1 int variable is required for 32 bits
+        // and 1 long int variable for 64 Bits boolean array operations
+
+        //This is the Most Optimised Solution
+
+        if(qpsf == NoQ){
+            System.out.println(ans);
+            return 1;
+        }
+        int count=0;
+
+        int m = boxes.length;
+        int n = boxes[0].length;
+
+        for(int c = 0; c<n; c++ ){
+            int r = qpsf;
+            // int c = i%boxes[0].length;
+
+            if(((rows & (1<<r)) == 0) && ((cols & (1<<c)) == 0) && 
+            ((mainD & (1<<(r+c))) == 0) && ((otherD & (1<<(r-c + (m+n-1)/2))) == 0)){
+                rows ^= (1<<r);
+                cols ^=(1<<c); 
+                mainD ^= (1<<(r+c));
+                otherD ^= (1<<(r-c + (m+n-1)/2));
+                // boxes[r][c] = 1;
+
+                //idx in the parameters is of no use now.
+
+                count+= NQueenCombNoKill_Summation_04Bits(NoQ, boxes, idx+1, qpsf+1, ans + "b"+r+""+c + "q"+qpsf + " - ",rows,cols,mainD,otherD);
+                // boxes[r][c] = 0;
+                rows ^= (1<<r);
+                cols ^=(1<<c);
+                mainD ^= (1<<(r+c));
+                otherD ^= (1<<(r-c + (m+n-1)/2));
+                                
+            }
+        }
+
+        return count;
+    }
+
+
+    public static int NQueenCombNoKill_Summation_03(int NoQ, int[][] boxes, int idx, int qpsf, String ans,
+    boolean[] rows, boolean[] cols, boolean[] mainD, boolean[] otherD) {
+        calls++;
+        //Optimised Function wihtout needing to use isSafeToPlaceQueen function
+        //Don't even need the 2D Box now
+        
+        //To make this function for Permutation, 
+        //just replace int i = 0 instead of idx, basically idx becomes obsolete
+        
+        //Addition to previous codes, there's one queen per row.
+        //Boxes array is still useless here.
+        //Mapping: Q0 => R0
+        //Q1 => R1
+        //and so on 
+        //QPSF variable can be used to keep this track.
+
+        if(qpsf == NoQ){
+            System.out.println(ans);
+            return 1;
+        }
+        int count=0;
+
+        int m = boxes.length;
+        int n = boxes[0].length;
+
+        for(int c = 0; c<n; c++ ){
+            int r = qpsf;
+            // int c = i%boxes[0].length;
+
+            if(!rows[r] && !cols[c] && !mainD[r+c] && !otherD[r-c + (m+n-1)/2]){
+                rows[r] = !rows[r];
+                cols[c] = !cols[c];
+                mainD[r+c] = !mainD[r+c];
+                otherD[r-c + (m+n-1)/2] = !otherD[r-c + (m+n-1)/2];
+                // boxes[r][c] = 1;
+
+                //idx in the parameters is of no use now.
+
+                count+= NQueenCombNoKill_Summation_03(NoQ, boxes, idx+1, qpsf+1, ans + "b"+r+""+c + "q"+qpsf + " - ",rows,cols,mainD,otherD);
+                // boxes[r][c] = 0;
+                rows[r] = !rows[r];
+                cols[c] = !cols[c];
+                mainD[r+c] = !mainD[r+c];
+                otherD[r-c + (m+n-1)/2] = !otherD[r-c + (m+n-1)/2];                
+            }
+        }
+
+        return count;
+    }
+
+
+    public static int NQueenCombNoKill_Subseq_02(int NoQ, int[][] boxes, int idx, int qpsf, String ans,
+    boolean[] rows, boolean[] cols, boolean[] mainD, boolean[] otherD) {
+        calls++;
+        
+        //Optimised Function wihtout needing to use isSafeToPlaceQueen function
+        //Don't even need the 2D Box now
+        
+        //To make this function for Permutation, 
+        //just replace int i = 0 instead of idx, basically idx becomes obsolete
+        if( qpsf == NoQ || idx == boxes.length * boxes[0].length){
+            if(qpsf == NoQ){
+                System.out.println(ans);
+                return 1;
+            }
+            return 0;
+        }
+        int count=0;
+
+        int m = boxes.length;
+        int n = boxes[0].length;
+        int r = idx/boxes[0].length;
+        int c = idx%boxes[0].length;
+
+        if(!rows[r] && !cols[c] && !mainD[r+c] && !otherD[r-c + (m+n-1)/2]){
+            rows[r] = !rows[r];
+            cols[c] = !cols[c];
+            mainD[r+c] = !mainD[r+c];
+            otherD[r-c + (m+n-1)/2] = !otherD[r-c + (m+n-1)/2];
+            // boxes[r][c] = 1;
+            count+= NQueenCombNoKill_Subseq_02(NoQ, boxes, idx+1, qpsf+1, ans + "b"+r+""+c + "q"+qpsf + " - ",rows,cols,mainD,otherD);
+            // boxes[r][c] = 0;
+            rows[r] = !rows[r];
+            cols[c] = !cols[c];
+            mainD[r+c] = !mainD[r+c];
+            otherD[r-c + (m+n-1)/2] = !otherD[r-c + (m+n-1)/2];                
+        }
+        
+            count+= NQueenCombNoKill_Subseq_02(NoQ, boxes, idx+1, qpsf, ans,rows,cols,mainD,otherD);
+        
+    
+
+        return count;
+    }
+
+
     public static int NQueenCombNoKill_Summation_02(int NoQ, int[][] boxes, int idx, int qpsf, String ans,
     boolean[] rows, boolean[] cols, boolean[] mainD, boolean[] otherD) {
+        calls++;
+        
         //Optimised Function wihtout needing to use isSafeToPlaceQueen function
         //Don't even need the 2D Box now
         
@@ -94,6 +281,8 @@ class l003_PnC{
     }
 
     public static int NQueenPermNoKill_Summation(int NoQ, int[][] boxes, int idx, int qpsf, String ans) {
+        calls++;
+        
         //For Permutation, You Gotta check all 8 directions instead of just 4 like in COmbination
         //Send False for Permutation
         if(qpsf == NoQ){
@@ -117,6 +306,8 @@ class l003_PnC{
     }
 
     public static int NQueenCombNoKill_Summation(int NoQ, int[][] boxes, int idx, int qpsf, String ans) {
+        calls++;
+        
         if(qpsf == NoQ){
             System.out.println(ans);
             return 1;
@@ -292,7 +483,7 @@ class l003_PnC{
 
     public static int CombWithRepDP(int[] arr, int tar ) {
         //Leetcode 518
-        //Not in compliance with the Video lectures. A step further basically
+        //Not in compliance with the Videos. A step further basically
         int m = arr.length;
         int n = tar+1;
         int[][] dp = new int[m][n];
---
+++ 1.Recursion/l005_Bits.java
@@ -0,0 +1,8 @@
+class l005_Bits{
+    public static void main(String[] args) {
+        bitOperations();
+    }
+    public static void bitOperations() {
+          
+    }
+}
